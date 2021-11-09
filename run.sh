396626a302eb20f37af233c36b1617032b33a86e
Michael Marsh <mmarsh@cs.umd.edu>
2021-11-09 10:38:06 -0500

    updating scripts for running task 2
    For some reason, the commented-out line in constrained_encrypt2
    doesn't register as a comment, so I've removed it. I've also updated
    the memory restriction flags for docker run to 6m from 4m, since
    docker changed the minimum value for some reason.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker run --privileged --rm --memory="4m" --memory-swap="4m" -v "$(pwd):/opt" -w /opt baseline ./constrained_encrypt2 $@
+docker run --privileged --rm --memory="6m" --memory-swap="6m" -v "$(pwd):/opt" -w /opt baseline ./constrained_encrypt2 $@
 
