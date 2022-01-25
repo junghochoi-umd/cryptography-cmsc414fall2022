c97c7f2568f2941bdf062cc1faa06187e8189546
Michael Marsh <mmarsh@cs.umd.edu>
2022-01-25 12:58:13 -0500

    more unnecessary files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker run --rm -v "$(pwd):/opt" baseline make -C /opt
-
--- run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker run --privileged --rm --memory="6m" --memory-swap="6m" -v "$(pwd):/opt" -w /opt baseline ./constrained_encrypt2 $@
-
